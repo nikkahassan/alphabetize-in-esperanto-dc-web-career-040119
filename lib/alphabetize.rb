81eda408fea47b49ba500f723e7a2c9fdd5300b3
Nikka Hassan <nikka.hassan@gmail.com>
2019-03-25 22:35:03 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,7 @@
+require 'pry'
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  arr.sort_by{|string| string.tr(ESPERANTO_ALPHABET, ascii)}
 end
